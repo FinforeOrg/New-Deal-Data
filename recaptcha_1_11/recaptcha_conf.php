f75aed5718cbdcf8aad56c806d6606cbf725cfa5
s n g <devsubhro@gmail.com>
2012-07-02 15:35:41 +0530

    adding recaptcha lib

---
+++ recaptcha_1_11/recaptcha_conf.php
@@ -0,0 +1,9 @@
+<?php
+$recaptcha_public_key = "6Lf7sMsSAAAAANAaj7Kp6DBjqY5nhKyk_RnGDUc6";
+$recaptcha_private_key = "6Lf7sMsSAAAAAIRmr52MD2s_Ic2RhHPCkHKJODn7";
+?>
+<script type="text/javascript">  
+var RecaptchaOptions = {  
+   theme : 'clean'  
+};  
+</script>
